f9771b3639464cb7bc7ff32ccb94567b5acaac78
daveclinton <clintondavid46@gmail.com>
2024-01-01 13:08:52 +0300

    fix: Add contenet

--- app/Main.tsx
+++ app/Main.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import Link from '@/components/Link'
 import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
@@ -12,10 +13,16 @@ export default function Home({ posts }) {
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Latest
+            Hey there, I'm David Clinton
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            {siteMetadata.description}
+            Welcome to my digital garden where I write about many things, including, but not limited
+            to React, Typescript, Javascript, Chakra UI, many more software engineering topics. You can learn
+            more about my professional experience in my
+            <Link href="/about" className="text-primary-500">
+              {` About `}
+            </Link>
+            section
           </p>
         </div>
         <ul className="divide-y divide-gray-200 dark:divide-gray-700">
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import 'css/tailwind.css'
 import 'pliny/search/algolia.css'
 
@@ -73,7 +74,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
       <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-      <body className="bg-white text-black antialiased dark:bg-gray-950 dark:text-white">
+      <body className="bg-sky-100 text-black antialiased dark:bg-[radial-gradient(ellipse_at_right,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black dark:text-white">
         <ThemeProviders>
           <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} />
           <SectionContainer>
