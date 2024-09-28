b636f9a717588bc4f3fc85a82df49cd8e14a41da
daveclinton <clintondavid46@gmail.com>
2024-09-28 15:46:48 +0300

    feat: remove socila themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,11 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import { siteMetadata } from "@/data/siteMetadata";
+import { Analytics, AnalyticsConfig } from "pliny/analytics";
+import { SearchProvider, SearchConfig } from "pliny/search";
+import SectionContainer from "@/components/SectionContainer";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,10 +19,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className="bg-gray-900 from-gray-700 via-gray-900 to-black text-black antialiased dark:bg-[radial-gradient(ellipse_at_right,_var(--tw-gradient-stops))] dark:text-white">
+        <Analytics
+          analyticsConfig={siteMetadata.analytics as AnalyticsConfig}
+        />
+        <SectionContainer>
+          {" "}
+          <div className="flex h-screen flex-col justify-between font-sans">
+            <SearchProvider searchConfig={siteMetadata.search as SearchConfig}>
+              <Header />
+              <main className="mb-auto">{children}</main>
+            </SearchProvider>
+            <Footer />
+          </div>
+        </SectionContainer>
       </body>
     </html>
   );
