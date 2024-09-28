7f3da791e115ffbf728ae7a62ca5371f5fc53605
daveclinton <clintondavid46@gmail.com>
2024-09-28 16:52:34 +0300

    feat: add mdx changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { siteMetadata } from "@/data/siteMetadata";
-import { Analytics, AnalyticsConfig } from "pliny/analytics";
-import { SearchProvider, SearchConfig } from "pliny/search";
 import SectionContainer from "@/components/SectionContainer";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
@@ -20,16 +17,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="bg-gray-900 from-gray-700 via-gray-900 to-black text-black antialiased dark:bg-[radial-gradient(ellipse_at_right,_var(--tw-gradient-stops))] dark:text-white">
-        <Analytics
-          analyticsConfig={siteMetadata.analytics as AnalyticsConfig}
-        />
         <SectionContainer>
-          {" "}
           <div className="flex h-screen flex-col justify-between font-sans">
-            <SearchProvider searchConfig={siteMetadata.search as SearchConfig}>
-              <Header />
-              <main className="mb-auto">{children}</main>
-            </SearchProvider>
+            <Header />
+            <main className="mb-auto">{children}</main>
+
             <Footer />
           </div>
         </SectionContainer>
--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,17 +1,12 @@
 import type { MDXComponents } from "mdx/types";
-import TOCInline from "pliny/ui/TOCInline";
-import Pre from "pliny/ui/Pre";
-import BlogNewsletterForm from "pliny/ui/BlogNewsletterForm";
+
 import Image from "./components/Image";
 import TableWrapper from "./components/TableWrapper";
 import CustomLink from "./components/Link";
 
 export const components: MDXComponents = {
   Image,
-  TOCInline,
-  pre: Pre,
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   a: CustomLink as any,
   table: TableWrapper,
-  BlogNewsletterForm,
 };
