7a3996f5052b0710fecd82a6ada4dcb7b310cc05
daveclinton <clintondavid46@gmail.com>
2024-09-28 15:05:00 +0300

    feat: add homepage

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,9 @@ import createMDX from '@next/mdx'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    pageExtensions:['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
-};
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+}
 
 const withMDX = createMDX({})
 
-export default withMDX(nextConfig);
+export default withMDX(nextConfig)
