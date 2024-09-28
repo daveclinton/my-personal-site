83a891d6aae715b822b00a3ab1719d1ada1aa952
daveclinton <clintondavid46@gmail.com>
2024-09-28 16:06:41 +0300

    feat: add pages

---
+++ app/main.tsx
@@ -0,0 +1,96 @@
+import Image from "@/components/Image";
+import Link from "@/components/Link";
+import { Rss, Github } from "lucide-react";
+import SocialIcon from "@/components/social-icons";
+import CustomLink from "@/components/Link";
+import EnhancedEmailSection from "@/components/EmailEnhanced";
+import { siteMetadata } from "@/data/siteMetadata";
+
+export const Home = () => {
+  return (
+    <>
+      <div>
+        <div className="mx-auto  overflow-hidden rounded-lg bg-gray-900 shadow-lg">
+          <div className="space-y-4 p-6">
+            <div className="flex items-center space-x-4">
+              <div className="relative">
+                <div className="absolute inset-0 animate-ping rounded-full bg-red-400 opacity-75"></div>
+                <Image
+                  src="/static/images/davecl.jpg"
+                  alt="Profile"
+                  className="relative h-16 w-16 rounded-full"
+                  width={50}
+                  height={50}
+                />
+              </div>
+              <div>
+                <h2 className="text-xl font-bold text-white">David Clinton</h2>
+                <p className="text-gray-400">Mobile & Frontend Engineer</p>
+              </div>
+            </div>
+            <div className="space-y-2">
+              <EnhancedEmailSection />
+              <p className="text-gray-300">
+                Need consulting? Book a call with me{" "}
+                <CustomLink
+                  href="https://cal.com/david-clinton-ucuv7n"
+                  className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                >
+                  here
+                </CustomLink>
+              </p>
+            </div>
+            <div className="flex justify-end space-x-4">
+              <CustomLink
+                href="https://daveclintonn.substack.com/"
+                className="text-gray-400 hover:text-white"
+              >
+                <Rss size={24} />
+              </CustomLink>
+              <SocialIcon kind="twitter" href={siteMetadata.twitter} size={6} />
+              <CustomLink
+                href="https://github.com/daveclinton"
+                className="text-gray-400 hover:text-white"
+              >
+                <Github size={24} />
+              </CustomLink>
+            </div>
+          </div>
+        </div>
+        <div className="space-y-2 pb-8 pt-6 md:space-y-5">
+          <h1 className="text-xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-xl sm:leading-10 md:text-2xl md:leading-14">
+            Hey Legend 😊,
+          </h1>
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+            All things here are Software Engineering related! You can subscribe
+            to receive an article straight into your inbox every Sunday! on my
+            <Link
+              href="https://daveclintonn.substack.com/"
+              className="text-primary-500"
+            >
+              {` Substack. `}
+            </Link>
+            You can also check out my
+            <Link href="/projects" className="text-primary-500">
+              {` Projects `}
+            </Link>{" "}
+            Here
+            <br />
+            <br />
+            My posts are a collaborative space, so feel free to reply if: 🧰
+            There’s a topic you’re curious about and would love to read about on
+            my blog. 🪜 You know about good resources that could benefit fellow
+            aspiring Software Engineers. 😁 You just want to say hello. :)
+            <br />
+            <br />I will always be happy to share more with you. Check out my
+            about here; :)
+            <Link href="/about" className="text-primary-500">
+              {` About `}
+            </Link>
+            section.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
---
+++ components/EmailEnhanced.tsx
@@ -0,0 +1,41 @@
+"use client";
+import React, { useState } from "react";
+import { Copy } from "lucide-react";
+
+const EnhancedEmailSection = () => {
+  const [isCopied, setIsCopied] = useState(false);
+  const email = "clintondavid46@gmail.com";
+
+  const copyEmail = () => {
+    navigator.clipboard.writeText(email);
+    setIsCopied(true);
+    setTimeout(() => setIsCopied(false), 2000);
+  };
+
+  return (
+    <p className="flex items-center space-x-2 text-gray-300">
+      <span>
+        Looking to hire me? Email me @ {email}{" "}
+        <button
+          onClick={copyEmail}
+          className="rounded p-1 text-primary-500 transition-colors duration-200 hover:text-primary-400 focus:outline-none "
+          aria-label="Copy email address"
+        >
+          {isCopied ? (
+            <span className="text-green-500">✓</span>
+          ) : (
+            <Copy size={12} />
+          )}
+        </button>
+      </span>
+
+      {isCopied && (
+        <span className="animate-fade-in-out text-sm text-green-500">
+          Copied!
+        </span>
+      )}
+    </p>
+  );
+};
+
+export default EnhancedEmailSection;
