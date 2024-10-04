a97ed59397fef829531745e857b7f66ea5c31c7f
daveclinton <clintondavid46@gmail.com>
2024-10-04 23:02:35 +0300

    feat; add margin bottom

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ import { siteMetadata } from "@/data/siteMetadata";
 
 const Header = () => {
   return (
-    <header className="flex items-center justify-between py-10">
+    <header className="flex items-center justify-between py-3 mb-10">
       <div>
         <Link href="/" aria-label={siteMetadata.headerTitle}>
           <div className="flex items-center justify-between">
