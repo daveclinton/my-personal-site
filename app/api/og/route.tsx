a5a65f698511166e4a1aa45cc7055131a8e03b25
daveclinton <clintondavid46@gmail.com>
2024-10-06 12:44:59 +0300

    feat: modify routes

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -20,8 +20,8 @@ export async function GET(request: NextRequest) {
             height: "100%",
             width: "100%",
             display: "flex",
-            flexDirection: "column",
-            alignItems: "flex-start",
+            flexDirection: "row",
+            alignItems: "center",
             justifyContent: "center",
             backgroundColor: "#fff", // fallback color
             backgroundImage:
